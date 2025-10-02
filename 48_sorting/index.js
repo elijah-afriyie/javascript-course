eed098bff11e8a0e96ed06fdba7f6a33c233ef52
elijah-afriyie <elijah.afry@gmail.com>
2025-10-02 15:38:37 +0000

    Update Sorting

--- 48_sorting/index.js
+++ 48_sorting/index.js
@@ -1,4 +1,3 @@
-/* ======= EXAMPLE ======= */
 const arr = [
   { id: 2, name: "Aiden" },
   { id: 1, name: "Skylar" },
@@ -28,3 +27,29 @@ const sortByGpa = people.toSorted((a, b) => a.gpa - b.gpa);
 console.log(sortByUsername);
 console.log(sortByAge);
 console.log(sortByGpa);
+
+/* ======= NEW EXAMPLE ======= */
+const cars = [
+    { make: "Toyota", model: "Corolla", year: 2018, mileage: 45000, color: "blue", electric: false },
+    { make: "Tesla", model: "Model 3", year: 2022, mileage: 12000, color: "white", electric: true },
+    { make: "Ford", model: "Mustang", year: 2015, mileage: 70000, color: "red", electric: false },
+    { make: "BMW", model: "X5", year: 2020, mileage: 30000, color: "black", electric: false },
+    { make: "Nissan", model: "Leaf", year: 2021, mileage: 15000, color: "green", electric: true },
+    { make: "Chevrolet", model: "Bolt", year: 2019, mileage: 28000, color: "silver", electric: true },
+    { make: "Honda", model: "Civic", year: 2017, mileage: 54000, color: "grey", electric: false },
+];
+
+const carSortByMake = cars.toSorted((a, b) => a.make.localeCompare(b.make));
+const carSortByModel = cars.toSorted((a, b) => a.model.localeCompare(b.model));
+const carSortByYear = cars.toSorted((a, b) => a.year - b.year);
+const carSortByMileage = cars.toSorted((a, b) => a.mileage - b.mileage);
+const carSortByColor = cars.toSorted((a, b) => a.color.localeCompare(b.color));
+const carSortByElectric = cars.sort((a, b) => Number(a.electric) - Number(b.electric));
+const carSortByNonElectric = cars.sort((a, b) => Number(b.electric) - Number(a.electric));
+
+console.log(carSortByMake)
+console.log(carSortByModel)
+console.log(carSortByYear)
+console.log(carSortByMileage)
+console.log(carSortByColor)
+console.log(carSortByElectric)
